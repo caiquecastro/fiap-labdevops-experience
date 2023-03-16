408e07b9ee7288dc782384a4b1e1a9ce7e5928a7
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2023-03-16 20:37:54 -0300

    Change index message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World FIAP"
+    return "Hello World FIAP - Aula 04 - Arquitetura Cloud Native"
 
 if __name__ == '__main__':
     app.run()
